aa118be739163105bde2385e763f4a5861cbbd1b
Frank Kaumba <engeezgamer@gmail.com>
2025-01-28 13:07:28 +0200

    Add files via upload

---
+++ plugins/script.js
@@ -0,0 +1,73 @@
+/*╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺
+    ⭐ＰＲＯＪＥＣＴ ＮＡＭＥ:
+    ＳＵＢＺＥＲＯ ＷＨＡＴＳＡＰＰ ＭＤ ＢＯＴ
+    
+    ⭐ＤＥＶＥＬＯＰＥＲ
+     ＭＲ ＦＲＡＮＫ 
+     
+    ⭐ ＭＹ ＴＥＡＭ
+     ＸＥＲＯ ＣＯＤＥＲＳ
+     
+    ⭐ ＯＵＲ ＷＥＢＳＩＴＥ
+     https://github.com/ZwSyntax/SUBZERO-MD
+
+© ＴＲＹ ＤＥＣＲＹＰＴＩＮＧ ＩＦ ＹＯＵ ＣＡＮ⚠
+
+╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺*/
+
+
+
+
+
+
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+    pattern: "script",
+    alias: ["sc","repo","info"],
+    desc: "bot repo",
+    react: "🤖",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let repo =`
+*╭──────────────●●►*
+> *BOT OWNER:*
+*|* *ᴍʀ ғʀᴀɴᴋ ᴏғᴄ*
+
+> *SUBZERO REPO:*
+*|* *https://github.com/zwsyntaxr/SUBZERO-MD*
+
+> *SUPPORT GROUP:*
+*|* *https://whatsapp.com/channel/*
+*╰──────────────●●►*
+
+> *CREATED BY MR FRANK*
+`
+await conn.sendMessage(from, { text: repo ,
+  contextInfo: {
+    mentionedJid: [ '' ],
+    groupMentions: [],
+    forwardingScore: 999,
+    isForwarded: false,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: '120363321386877609@newsletter',
+      newsletterName: "ᴋᴇʀᴍ ᴄʜᴀɴɴᴇʟ",
+      serverMessageId: 999
+    },
+externalAdReply: { 
+title: 'Kᴇʀᴍ_Mᴅ-V4',
+body: `${pushname}`,
+mediaType: 1,
+sourceUrl: "https://github.com/ZwSyntax/SUBZERO-MD" ,
+thumbnailUrl: "https://files.catbox.moe/osriwc.jpg" ,
+renderLargerThumbnail: true,
+showAdAttribution: true
+}
+}}, { quoted: mek})}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+});
